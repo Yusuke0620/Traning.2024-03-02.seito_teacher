1ec11a96d972d92ea38e3fe29ec20172e8dca19b
Yusuke0620 <boston19851229@gmail.com>
2024-03-02 21:23:09 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const man = {
+    name: "John",
+    age: 35,
+    gender: "man",
+    family: ["母", "父", "犬"],
+    from: 'japan',
+}
+
+
+
+console.log(`男性の家族構成は、自分と${man.family[0]}と${man.family[1]}と${man.family[2]}の${man.family.length}人(匹)です`);
+
+console.log("男性の家族構成は、自分と" + man.family[0] + "と" + man.family[1] + "と" + man.family[2] + "の" + man.family.length + "人(匹)です");
+
+
+
+
